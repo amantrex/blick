fe3398b89f65e7bee4d5142b752c114dd8a0b6c2
Aman Jain <aj.amanjain1993@gmail.com>
2025-08-22 13:08:44 +0530

    sign in form

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,18 +1,22 @@
 import { SignIn } from "@clerk/nextjs";
+import Link from "next/link";
 
 export default function Page() {
   return (
-    <div className="flex justify-center items-center min-h-screen">
+    <div className="flex justify-center items-center min-h-screen flex-col">
       <SignIn
         signUpUrl="/sign-up"
         appearance={{
           elements: {
-            footerActionLink: {
-              display: 'block',
+            footer: {
+              display: "none",
             },
           },
         }}
       />
+      <div className="mt-4">
+        Don't have an account? <Link href="/sign-up">Sign up</Link>
+      </div>
     </div>
   );
 }
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,22 @@
 import { SignUp } from "@clerk/nextjs";
+import Link from "next/link";
 
 export default function Page() {
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <SignUp />
+    <div className="flex justify-center items-center min-h-screen flex-col">
+      <SignUp
+        signInUrl="/sign-in"
+        appearance={{
+          elements: {
+            footer: {
+              display: "none",
+            },
+          },
+        }}
+      />
+      <div className="mt-4">
+        Already have an account? <Link href="/sign-in">Sign in</Link>
+      </div>
     </div>
   );
 }
